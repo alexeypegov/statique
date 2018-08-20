8152a41278b1fc2bcbfd9d1cd32ba30922d513fb
Alexey Pegov <iam@alexeypegov.com>
2018-08-20 21:38:51 +0300

    fix FS to use root-dir as source for getting fs cache file keys

--- src/statique/fs.clj
+++ src/statique/fs.clj
@@ -8,6 +8,7 @@
 
 (defprotocol BlogFileSystem
   (copy [this src dst-dir])
+  (info [this file])
   (save [this]))
 
 (defn- list-files
@@ -17,22 +18,25 @@
       (filter #(not (.isDirectory %)) (file-seq file))
       (list file))))
 
-(defn- info
-  [src dst cache]
+(defn- file-info
+  [root-dir file cache]
+  (let [relative  (u/relative-path root-dir file)]
+    (assoc {}
+      :relative relative
+      :timestamp (.lastModified file)
+      :cached-timestamp (get (:read cache) relative 0)
+      :src file)))
+
+(defn- copy-info-fn
+  [root-dir src dst cache]
   (fn [file]
-    (let [ts        (.lastModified file)
-          relative  (u/relative-path (.getParent src) file)
-          cached-ts (get (:read cache) relative 0)
-          dst       (io/file dst relative)
-          exists    (.exists dst)]
-      (assoc {}
-        :relative relative
-        :timestamp ts
-        :cached-timstamp cached-ts
+    (let [info      (file-info root-dir file cache)
+          dst-file  (io/file dst (u/relative-path (.getParent src) file))
+          exists    (.exists dst-file)]
+      (assoc info
         :exists exists
-        :outdated (or (not exists) (not= ts cached-ts))
-        :src file
-        :dst dst))))
+        :outdated (or (not exists) (not= (:timestamp info) (:cached-timestamp info)))
+        :dst dst-file))))
 
 (def copy
   (fn [m]
@@ -40,18 +44,18 @@
       (fs/copy+ (:src m) (:dst m)))
     m))
 
-(defn- update-cache
+(defn- update-cache-fn
   [cache]
   (fn [m]
     (swap! (:write cache) assoc (:relative m) (:timestamp m))
     m))
 
 (defn- make-copier
-  [src dst cache]
+  [root-dir src dst cache]
   (comp
-    (update-cache cache)
+    (update-cache-fn cache)
     copy
-    (info src dst cache)))
+    (copy-info-fn root-dir src dst cache)))
 
 (defn make-fs
   [root-dir cache-dir]
@@ -59,9 +63,10 @@
         cache       {:read (u/read-edn cache-file) :write (atom {})}]
     (reify BlogFileSystem
       (copy [this src dst_dir]
-            (let [copier (make-copier src dst_dir cache)]
+            (let [copier (make-copier root-dir src dst_dir cache)]
               (reduce #(if (:outdated %2) (inc %1) %1) 0
                 (map copier (list-files src)))))
+      (info [_ file] (file-info root-dir file cache))
       (save [this]
             (.mkdirs cache-dir)
             (spit cache-file (with-out-str (pr @(:write cache))))
